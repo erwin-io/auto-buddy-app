34b044dfe548c458d9e047b25c009e94cd4c2118
erwin-io <erwinramirez220@gmail.com>
2024-03-17 10:42:55 +0800

    changes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
+const axios = require('axios');
 
+let config = {
+  method: 'get',
+  maxBodyLength: Infinity,
+  url: 'https://apisearch.topgear.com.ph/topgear/v1/buyers-guide/search-vehicles/motorcycle?keywords=honda&filterType=vehicle_name&match=wildcard',
+  headers: { 
+    'Origin': 'https://www.topgear.com.ph'
+  }
+};
 
-export default function handler(request, response) {
-  const { name = 'World' } = request.query;
-  return response.send(`Hello ${name}!`);
-}
+axios.request(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
